14bdb125bf7b7d4d840889b88a2a4e1f3f5446c8
jime <kuritahajime.business@gmail.com>
2025-01-07 04:53:52 +0900

    サービスページ文章入れ、デザイン作成

--- service.php
+++ service.php
@@ -9,75 +9,45 @@ Template Name: service
 
 
 <div class="service-top">
-            <div class="company-top-img">
+            <div class="service-top-img">
                 <h2>service</h2>
             </div>
         </div>
 
         <div class="top-page-greeting">
-            <h2>ビジョン</h2>
-            <h3 class="title">VISION</h3>
-            <p class="p-t-20">株式会社 Jime Companyは、最先端技術を取り入れながら、世の中の変化に柔軟に対応し、<br>
-                技術力 x 信頼力でお客様へ最適なサービスのご提供と社会貢献をしていきます。</p>
-            <p class="p-t-20">仕事は誰にでもできるかもしれない、しかしお客様との信頼関係を築くことはその人の「人間力」が必要になる。</p>
-            <p class="p-t-20">「あなたに仕事を任せたい。」と言われるような人材育成とチーム体制の向上を目指していきます。</p>
+            <h2>事業案内</h2>
+            <h3 class="title">SERVICE</h3>
+            <p class="p-t-20">当社の事業をご紹介します</p>
         </div>
 
-        <div class="about-wrapper">
-            <h2>会社概要</h2>
-            <h3 class="title">About Us</h3>
-
-            <div class="about-contents">
-                <div class="abaout-content">
-                    <div class="about-left"><p>社名</p></div>
-                    <div class="about-right"><p>株式会社 Jime Company</p></div>
+        <div class="service-contents">
+            <div class="service-content">
+                <img src="/img/pc2.jpg" alt="">
+                <div class="service-right">
+                    <h2>DEVELOPMENT SERVICE 受託開発サービス</h2>
+                    <p>当社では、主にスマートフォン向けソフトウェアの受託開発を行っています。 
+                        多くの実績で培った制御フローや品質向上への取り組み、その後の迅速なサポートなど、
+                        お客様の状況に応じた技術を提供致します。
+                        低予算で速度を重視するようなニーズでもより良いものが作れるよう、お客様と一緒に開発を行っています。</p>
                 </div>
-
-                <div class="abaout-content">
-                    <div class="about-left"><p>資本金</p></div>
-                    <div class="about-right"><p>1000万円</p></div>
+                <div class="service-bottom">
+                    <div class="service-bottom-c">
+                        <h3>現行の主な実績</h3>
+                        <p>・カーナビアプリ開発（Android/iOS:Java,Objective-C)でそれぞれ4種類</p>
+                        <p>・公共系サービスアプリ開発（Android/iOS:Java,Objective-C)</p>
+                        <p>・インフラ情報表示アプリ開発(AWS:Java/JavaScript/PHP)</p>
+                        <p>・シミュレーション・情報管理アプリ開発（Android/iOS:Java,Objective-C)</p>
+                    </div>
+                    <div class="service-bottom-c">
+                        <h3>組込案件時の主な実績</h3>
+                        <p>・航空管制関連システム開発(Linux,LinuxWK,Solaris,AIX,HP-UX:C/C++)</p>
+                        <p>・防衛庁関連システム開発(VXWorks:C/C++)</p>
+                        <p>・天然ガス秤量システム開発(Windows:Microsoft VisualStudio2003 C/C++)</p>
+                        <p>・TV関連開発(Linux:C/C++)</p>
+                    </div>
                 </div>
-
-                <div class="abaout-content">
-                    <div class="about-left"><p>代表者</p></div>
-                    <div class="about-right"><p>代表取締役社長    栗田孟</p></div>
-                </div>
-
-                <div class="abaout-content">
-                    <div class="about-left"><p>所在地</p></div>
-                    <div class="about-right"><p>〒000-0000<br>東京都板橋区〇〇〇〇 〇〇〇〇ビル7F</p></div>
-                </div>
-
-                <div class="abaout-content">
-                    <div class="about-left"><p>取引銀行</p></div>
-                    <div class="about-right"><p>〇〇〇銀行 〇〇支店<br>△△△銀行 △△支店</p></div>
-                </div>
-
-                <div class="abaout-content">
-                    <div class="about-left"><p>加盟団体</p></div>
-                    <div class="about-right"><p>関東ITソフトウェア健康保険組合</p></div>
-                </div>
-
-                <div class="abaout-content">
-                    <div class="about-left"><p>顧問弁護士</p></div>
-                    <div class="about-right"><p>〇〇〇法律事務所</p></div>
-                </div>
-
-                <div class="abaout-content">
-                    <div class="about-left"><p>顧問税理士</p></div>
-                    <div class="about-right"><p>税理士法人△△△</p></div>
-                </div>
-
-                <div class="abaout-content">
-                    <div class="about-left"><p>顧問社労士</p></div>
-                    <div class="about-right"><p>社会保険労務士法人〇〇〇</p></div>
-                </div>
-
-                <div class="top-page-btn m-b-0 m-t-50">
-                    <a href="">アクセス</a>
-                </div>
-
             </div>
+        </div>
 </div>
 
 
