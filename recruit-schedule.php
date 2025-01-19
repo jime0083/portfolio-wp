f027facacdf0532253ffb1702419d473bdf28359
jime <kuritahajime.business@gmail.com>
2025-01-20 02:56:20 +0900

    社員の1日作成

---
+++ recruit-schedule.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: schedule
+*/
+?>
+
+
+<?php get_header(4); ?>
+
+
+<div class="part-top">
+            <div class="part-top-img">
+                <h2>part-time-job</h2>
+            </div>
+        </div>
+
+        <div class="recruit-page-greeting">
+            <h2>パート・アルバイト</h2>
+
+            <div class="works">
+
+                <div class="work">
+                    <a href="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/entry.jpg" alt="">
+                        <p>バックオフィス</p>
+                    </a>
+                </div>
+                
+            </div>
+        </div>
+
+</div>
+
+
+<?php get_footer(2); ?>
