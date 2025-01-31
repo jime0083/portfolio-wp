082dfcad3c1e7f03382569e51c3dbce9d79efe38
jime <kuritahajime.business@gmail.com>
2025-02-01 00:49:51 +0900

    headerレスポンシブ途中

--- header.php
+++ header.php
@@ -24,6 +24,22 @@
                     <a class="header-link header-link-last" href="http://portfolio.local/contact/">CONTACT</a>
                 </div>
             </div>
-        </div>
+    </div>
 
-    
+
+
+
+    <div class="header-respo">
+            <div class="header-content">
+                <div class="header-links">
+                    <a class="header-logo"><img class="header-img" src="<?php echo get_template_directory_uri(); ?>/img/header-logo.png" alt="" ></a>
+                    <a class="header-link" href="http://portfolio.local/">HOME</a>
+                    <a class="header-link" href="http://portfolio.local/news/">NEWS</a>
+                    <a class="header-link" href="http://portfolio.local/company/">COMPANY</a>
+                    <a class="header-link" href="http://portfolio.local/service/">SERVICE</a>
+                    <a class="header-link" href="http://portfolio.local/recruit-top/">RECRUIT</a>
+                    <a class="header-link" href="http://portfolio.local/access/">ACCESS</a>
+                    <a class="header-link header-link-last" href="http://portfolio.local/contact/">CONTACT</a>
+                </div>
+            </div>
+    </div>
